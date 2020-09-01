96d5b7acd26357aa13cd04975a0a3f88c992ae32
Harsh Virani <iamharshvirani7@gmail.com>
2020-09-01 02:11:12 -0700

    Books Create/Index/New

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({path:'./.env'});
+  require('dotenv').config({path:'./.env'});
 }
 
 const express = require('express')
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -20,10 +21,11 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-db.on('error', error => console.log(error))
-db.on('open', error => console.log('Conencted to Mongoose'))
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
